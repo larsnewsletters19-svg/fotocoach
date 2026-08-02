15927dc76a2cf320ca8211aed9bb37212f5a50a0
Lars Westerblom <lars.westerblom@gmail.com>
2026-08-02 13:37:24 +0200

    perf: v0.6.1 – snabbare analys

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = '0.6.0';
-export const APP_VERSION_LABEL = '0.6 – Ljust/mörkt tema & mobilkameror';
+export const APP_VERSION = '0.6.1';
+export const APP_VERSION_LABEL = '0.6.1 – Snabbare analys';
